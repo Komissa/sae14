859437bfa17b56b20da7cb19aa7165724be04686
Komissa <83428817+Komissa@users.noreply.github.com>
2022-01-17 16:28:21 +0100

    sae

--- src/Controller/YaoController.php
+++ src/Controller/YaoController.php
@@ -26,4 +26,41 @@ class YaoController extends AbstractController
             'controller_name' => 'YaoController',
         ]);
     }
+    /**
+     * @Route("/bibliographie", name="bibliographie")
+     */
+    public function bibliographie(): Response
+    {
+        return $this->render('yao/bibliographie.html.twig', [
+            'controller_name' => 'YaoController',
+        ]);
+    }
+    /**
+     * @Route("/Acceuil", name="Acceuil")
+     */
+    public function Acceuil(): Response
+    {
+        return $this->render('yao/Acceuil.html.twig', [
+            'controller_name' => 'YaoController',
+        ]);
+    }
+     /**
+     * @Route("/Portfolio", name="Portfolio")
+     */
+    public function Pt(): Response
+    {
+        return $this->render('yao/Portfolio.html.twig', [
+            'controller_name' => 'YaoController',
+        ]);
+    }
+    /**
+     * @Route("/cv", name="cv")
+     */
+    public function cv(): Response
+    {
+        return $this->render('yao/cv.html.twig', [
+            'controller_name' => 'YaoController',
+        ]);
+    }
+       
 }
